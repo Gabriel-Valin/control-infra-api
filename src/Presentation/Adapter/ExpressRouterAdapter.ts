90193e2fb36e480de8f5fe6d9e7bbb0d96edd16a
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-11-24 09:30:45 -0300

    feat: add express router adapter

---
+++ src/Presentation/Adapter/ExpressRouterAdapter.ts
@@ -0,0 +1,16 @@
+
+import { Controller } from "@/Presentation/Protocol/Controller"
+import { RequestHandler } from "express"
+
+type Adapter = (controller: Controller) => RequestHandler
+
+export const adaptExpressRoute: Adapter = (controller) => async (req, res) => {
+  const { statusCode, data } = await controller.handle({
+    ...req.body,
+    ...req.headers,
+    ...req.params
+  })
+  const json = [200, 201].includes(statusCode) ? data : { error: data?.message }
+
+  res.status(statusCode).json(json)
+}
