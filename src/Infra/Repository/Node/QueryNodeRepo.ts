d038ee924c9952a73171b8a1bbc27f7bbf10b631
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-11-25 11:38:55 -0300

    feat: remove string (v) from return

--- src/Infra/Repository/Node/QueryNodeRepo.ts
+++ src/Infra/Repository/Node/QueryNodeRepo.ts
@@ -1,12 +1,14 @@
 import { QueryNode } from "@/Domain/Usecase/GetNodeVersionInteractor";
 import { InfraError } from "@/Presentation/Error/InfraError";
-import { spawn } from 'child_process'
+import { spawn, exec } from 'child_process'
 import { once } from 'events'
+import { cwd } from "process";
 
 export default class QueryNodeRepo implements QueryNode {
     public async getNodeVersion(): Promise<{nodeVersion: string}> {
         try {
             const nodeVersion = spawn('node', ['--version'])
+
             let output = ''
 
             nodeVersion.stdout.setEncoding('utf-8')
@@ -17,7 +19,7 @@ export default class QueryNodeRepo implements QueryNode {
 
             await once(nodeVersion, 'close')
             return {
-                nodeVersion: output.replace(/[\r\n]/g, '')
+                nodeVersion: output.replace(/([\r\n]|v)/g, '')
             }
         } catch (error) {
             throw new InfraError("Have error on node version command")
