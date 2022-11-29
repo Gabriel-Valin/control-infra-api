19893ae4da39841d5c1f568abe3a589c50d65a5b
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-11-29 17:54:06 -0300

    refactor: change expect from test

--- tests/Infra/Node/CommandRepoNode.spec.ts
+++ tests/Infra/Node/CommandRepoNode.spec.ts
@@ -1,4 +1,6 @@
 import CommandRepoNode from "@/Infra/Node/Repository/CommandRepoNode"
+import { InfraError } from "@/Presentation/Error/InfraError"
+import { ChildProcess } from 'child_process'
 
 const makeSut = (): CommandRepoNode => {
     const sut = new CommandRepoNode()
@@ -15,6 +17,15 @@ describe('CommandRepoNode', () => {
     it('should returns new default shell node version', async () => {
         const sut = makeSut()
         const nodeVersion = await sut.updateNodeVersion(18)
-        expect(nodeVersion).toHaveProperty('newNodeVersion')
+        expect(nodeVersion).toBeTruthy()
+    })
+
+    it('should returns infra error', async () => {
+        jest.spyOn(ChildProcess.prototype, 'once').mockImplementationOnce(() => {
+            throw new InfraError("Have error on node version command")
+        })
+        const sut = makeSut()
+        const promise = sut.updateNodeVersion(0)
+        await expect(promise).rejects.toEqual(new InfraError("Have error on node version command"))
     })
 })
---
+++ todo.md
@@ -0,0 +1,1 @@
+- [ ] Controller Update Version Tests
