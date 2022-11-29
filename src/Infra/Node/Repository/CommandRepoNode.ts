21ca877a95a6ba880900c4746df353a66d48de38
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-11-29 16:58:40 -0300

    feat: move class to src folder

---
+++ src/Infra/Node/Repository/CommandRepoNode.ts
@@ -0,0 +1,45 @@
+import { InfraError } from "@/Presentation/Error/InfraError"
+import { spawn } from 'child_process'
+import { once } from "events"
+import { cwd } from "process"
+
+export interface CommandRepo {
+    updateNodeVersion(version: number): Promise<{ newNodeVersion: string } | string>
+}
+
+export default class CommandRepoNode implements CommandRepo {
+    public async updateNodeVersion(version: number): Promise<{ newNodeVersion: string } | string> {
+        try {
+            let output = ''
+            
+            const updateNodeVersionByNVM = spawn(`./UpdateNodeVersion.sh ${version.toString()}`, {
+                cwd: `${cwd()}/shs/NODE`,
+                shell: '/bin/sh'
+            })
+
+            updateNodeVersionByNVM.stdout.setEncoding('utf-8')
+
+            updateNodeVersionByNVM.stdout.on('data', data => {
+                output += data.toString()
+            })
+
+            updateNodeVersionByNVM.stderr.on('data', data => {
+                output += data.toString()
+            })
+
+            await once(updateNodeVersionByNVM, 'close')
+
+            const regexInvalidVersion = new RegExp('N\/A')
+            const matchValue = regexInvalidVersion.test(output)
+           
+            if (matchValue) {
+                return 'Invalid Node Version, please install first or typing correctly version.'
+            }
+            return {
+                newNodeVersion: output.substring(69, 78)
+            }
+        } catch (error) {
+            throw new InfraError("Have error on node version command")
+        }
+    }
+}
--- tests/Infra/Node/CommandRepoNode.spec.ts
+++ tests/Infra/Node/CommandRepoNode.spec.ts
@@ -1,54 +1,9 @@
-import { InfraError } from '@/Presentation/Error/InfraError'
-import { spawn, exec, execSync } from 'child_process'
-import { once } from 'events'
-import { cwd } from 'process'
-
-export interface CommandRepo {
-    updateNodeVersion(version: number): Promise<{ newNodeVersion: string } | string>
-}
-
-export default class CommandRepoNode implements CommandRepo {
-    public async updateNodeVersion(version: number): Promise<{ newNodeVersion: string } | string> {
-        try {
-            let output = ''
-            
-            const nvmListVersions = spawn(`./UpdateNodeVersion.sh ${version.toString()}`, {
-                cwd: `${cwd()}/shs/NODE`,
-                shell: '/bin/sh'
-            })
-
-            nvmListVersions.stdout.setEncoding('utf-8')
-
-            nvmListVersions.stdout.on('data', data => {
-                output += data.toString()
-            })
-
-            nvmListVersions.stderr.on('data', data => {
-                output += data.toString()
-            })
-
-            await once(nvmListVersions, 'close')
-
-            const regexInvalidVersion = new RegExp('N\/A')
-            const matchValue = regexInvalidVersion.test(output)
-           
-            if (matchValue) {
-                return 'Invalid Node Version, please install first or typing correctly version.'
-            }
-            return {
-                newNodeVersion: output.substring(69, 78)
-            }
-        } catch (error) {
-            throw new InfraError("Have error on node version command")
-        }
-    }
-}
+import CommandRepoNode from "@/Infra/Node/Repository/CommandRepoNode"
 
 const makeSut = (): CommandRepoNode => {
     const sut = new CommandRepoNode()
     return sut
 }
-
 
 describe('CommandRepoNode', () => {
     it('should returns node error message if invalid version is provided', async () => {
