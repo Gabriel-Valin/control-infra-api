269de2c981fb79ab4f352d34f65410b08bf16e72
Gabriel-Valin <gabrielvalincontato@gmail.com>
2022-12-01 16:38:00 -0300

    feat: add trycatch block in docker repo

--- src/Infra/Docker/Repository/QueryPSCommand.ts
+++ src/Infra/Docker/Repository/QueryPSCommand.ts
@@ -1,3 +1,4 @@
+import { InfraError } from "@/Presentation/Error/InfraError"
 import { spawn } from "child_process"
 import { once } from "events"
 
@@ -7,21 +8,26 @@ export interface QueryPSDocker {
 
 export class QueryPSCommand implements QueryPSDocker {
     public async getContainers(all?: boolean): Promise<any[]> {
-        let output = ''
-        const formatResult = "--no-trunc --format '{{ json . }}' | jq -s 'map({id:.ID,network:.Networks,runningFor:.RunningFor,state:.State,name:.Names,status:.Status})'"
-        const allContainers = all ? `-a ${formatResult}` : formatResult
-        
-        const containersList = spawn('docker ps', [allContainers], {
-            shell: true
-        })
-        
-        containersList.stdout.setEncoding('utf-8')
-        containersList.stdout.on('data', data => {
-            output += data.toString()
-        })
+        try {
+            let output = ''
+            const formatResult = "--no-trunc --format '{{ json . }}' | jq -s 'map({id:.ID,network:.Networks,runningFor:.RunningFor,state:.State,name:.Names,status:.Status})'"
+            const allContainers = all ? `-a ${formatResult}` : formatResult
+            
+            const containersList = spawn('docker ps', [allContainers], {
+                shell: true
+            })
+            
+            containersList.stdout.setEncoding('utf-8')
+            containersList.stdout.on('data', data => {
+                output += data.toString()
+            })
 
-        await once(containersList, 'close')
-        const parseToObject = JSON.parse(output)
-        return Array.from(parseToObject)
+            await once(containersList, 'close')
+            const parseToObject = JSON.parse(output)
+            return Array.from(parseToObject)
+        } catch (error) {
+            throw new InfraError("Have error on node version command")
+        }
+        
     }
 }
